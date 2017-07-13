874d89ae42040ac5dd42e1656f781ac192698bdb
Brian <bbensch@gmail.com>
2017-07-13 00:22:21 -0700

    remove sign in gate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_action :authenticate_user!
+  # before_action :authenticate_user!
   before_action :set_user
   after_action :store_location
 
